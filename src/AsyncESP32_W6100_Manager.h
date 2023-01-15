f20021ce257fbbd50cb154fda416b78f31f18f1b
Khoi Hoang <57012152+khoih-prog@users.noreply.github.com>
2023-01-14 19:33:18 -0500

    v1.0.0 for`LwIP W6100 Ethernet`
    #### Releases v1.0.0
    
    1. Initial coding to port [ESPAsync_WiFiManager](https://github.com/khoih-prog/ESPAsync_WiFiManager) to ESP32 boards using `LwIP W6100 Ethernet`.
    2. Use `allman astyle`

---
+++ src/AsyncESP32_W6100_Manager.h
@@ -0,0 +1,31 @@
+/****************************************************************************************************************************
+  AsyncESP32_W6100_Manager.h
+
+  For Ethernet shields using ESP32_W6100 (ESP32 + W6100)
+
+  Modified from
+  1. Tzapu               (https://github.com/tzapu/WiFiManager)
+  2. Ken Taylor          (https://github.com/kentaylor)
+  3. Alan Steremberg     (https://github.com/alanswx/ESPAsyncWiFiManager)
+  4. Khoi Hoang          (https://github.com/khoih-prog/ESPAsync_WiFiManager)
+
+  Built by Khoi Hoang https://github.com/khoih-prog/AsyncESP32_W6100_Manager
+  Licensed under MIT license
+
+  Version: 1.0.0
+
+  Version Modified By  Date      Comments
+  ------- -----------  ---------- -----------
+  1.0.0   K Hoang     14/01/2023 Initial coding for ESP32_W6100
+ *****************************************************************************************************************************/
+
+#pragma once
+
+#ifndef AsyncESP32_W6100_Manager_h
+#define AsyncESP32_W6100_Manager_h
+
+#include <AsyncESP32_W6100_Manager.hpp>              //https://github.com/khoih-prog/AsyncESP32_W6100_Manager
+#include <AsyncESP32_W6100_Manager_Impl.h>           //https://github.com/khoih-prog/AsyncESP32_W6100_Manager
+
+#endif    // AsyncESP32_W6100_Manager_h
+
